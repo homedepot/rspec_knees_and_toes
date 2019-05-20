1017f02be77010c447c56fc5b6044f045c9f499c
Winston R. Milling <Winston_R_Milling@homedepot.com>
2019-05-20 09:53:10 -0400

    Added code coverage reporting
    Also added a few badges for the repo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'analyze_parallel_spec_logs'
